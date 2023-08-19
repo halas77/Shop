18554ead9f99cb4fe045c3f953af08c82c3714f6
Dawit <138193176+halas77@users.noreply.github.com>
2023-08-19 12:33:02 -0400

    Backgroud Img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Typed from "react-typed";
+import '../index.css'
 
 const Home = () => {
   return (
-    <div className= "text-white">
-      <div className="max-w-[1000px] mt-[-96] w-full h-screen mx-auto text-center flex flex-col justify-center">
+    <div className="text-white">
+      <div className="hero-container mt-[-90] w-full h-screen mx-auto text-center flex flex-col justify-center">
         <p className="text-[#00df9a] text-2xl font-bold p-2">STAY IN FASHION</p>
         <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 ">
           Keep with Fashion.
@@ -21,9 +22,9 @@ const Home = () => {
             loop
           />
         </div>
-        <p className="md:text-2xl text-xl font-bold text-gray-400 px-3">
-          Monitor your analytics to increase your confort with Brand, Smooth, & Quality
-          fashions.
+        <p className="md:text-2xl text-xl font-bold text-gray-100 px-3">
+          Monitor your analytics to increase your confort with Brand, Smooth, &
+          Quality fashions.
         </p>
         <button className="bg-[#00df9a]  w-[200px] rounded-md font-bold my-6 mx-auto py-3 text-black">
           Get Started
