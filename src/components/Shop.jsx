43c9bbffb67ecefd43fceb002b917e7f87156ed5
Dawit <138193176+halas77@users.noreply.github.com>
2023-08-19 16:22:40 -0400

    Shop component

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,19 +1,141 @@
 import React from "react";
-import Single from "../assets/shoe1.jpeg";
+import { items } from "../constant";
+import shoe1 from "../assets/shoe1.jpeg";
+import shoe2 from "../assets/shoe2.jpeg";
+import shoe3 from "../assets/tshirt1.png";
 
 const Shop = () => {
   return (
-    <div className="w-full py-[10rem] px-4 bg-white">
+    <div className="w-full py-[3rem] px-4 bg-white">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 sm:p-9 gap-8">
         <div className="w-full bg-[#080b1b] shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img
             className="w-200 mx-auto mt-[-5rem] rounded-xl"
-            src={Single}
+            src={shoe1}
             alt="/"
           />
-          <p className="text-center pb-4 border-gray-600 border-b mt-10 text-white text-4xl font-bold">$149</p>
+          <p className="text-center pb-4 border-gray-600 border-b mt-10 text-white text-4xl font-bold">
+            {items.map((link) => (
+              <p>{link.cost}</p>
+            ))}
+          </p>
           <div className="text-center font-medium">
-            <p className="py-2 mx-8 text-white font-bold mt-8">500 GB Storage</p>
+            <p className="py-2 mx-8 text-white font-bold mt-8">
+              {items.map((link) => (
+                <p>{link.desc}</p>
+              ))}
+            </p>
+          </div>
+          <button className="bg-[#00df9a] w-[200px] text-[#090d28] rounded-md font-bold my-6 mx-auto px-6 py-3">
+            Buy Now
+          </button>
+        </div>
+
+        <div className="w-full bg-[#080b1b] shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-200 mx-auto mt-[-5rem] rounded-xl"
+            src={shoe1}
+            alt="/"
+          />
+          <p className="text-center pb-4 border-gray-600 border-b mt-10 text-white text-4xl font-bold">
+            {items.map((link) => (
+              <p>{link.cost}</p>
+            ))}
+          </p>
+          <div className="text-center font-medium">
+            <p className="py-2 mx-8 text-white font-bold mt-8">
+              {items.map((link) => (
+                <p>{link.desc}</p>
+              ))}
+            </p>
+          </div>
+          <button className="bg-[#00df9a] w-[200px] text-[#090d28] rounded-md font-bold my-6 mx-auto px-6 py-3">
+            Buy Now
+          </button>
+        </div>
+        <div className="w-full bg-[#080b1b] shadow-xl flex flex-col p-4 my-4 mt- rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-200 mx-auto mt-[-5rem] rounded-xl"
+            src={shoe1}
+            alt="/"
+          />
+          <p className="text-center pb-4 border-gray-600 border-b mt-10 text-white text-4xl font-bold">
+            {items.map((link) => (
+              <p>{link.cost}</p>
+            ))}
+          </p>
+          <div className="text-center font-medium">
+            <p className="py-2 mx-8 text-white font-bold mt-8">
+              {items.map((link) => (
+                <p>{link.desc}</p>
+              ))}
+            </p>
+          </div>
+          <button className="bg-[#00df9a] w-[200px] text-[#090d28] rounded-md font-bold my-6 mx-auto px-6 py-3">
+            Buy Now
+          </button>
+        </div>
+        <div className="w-full bg-[#080b1b] shadow-xl flex flex-col p-4 my-4 rounded-lg mt-12 hover:scale-105 duration-300">
+          <img
+            className="w-200 mx-auto mt-[-5rem] rounded-xl"
+            src={shoe1}
+            alt="/"
+          />
+          <p className="text-center pb-4 border-gray-600 border-b mt-10 text-white text-4xl font-bold">
+            {items.map((link) => (
+              <p>{link.cost}</p>
+            ))}
+          </p>
+          <div className="text-center font-medium">
+            <p className="py-2 mx-8 text-white font-bold mt-8">
+              {items.map((link) => (
+                <p>{link.desc}</p>
+              ))}
+            </p>
+          </div>
+          <button className="bg-[#00df9a] w-[200px] text-[#090d28] rounded-md font-bold  my-6 mx-auto px-6 py-3">
+            Buy Now
+          </button>
+        </div>
+        <div className="w-full bg-[#080b1b] shadow-xl flex flex-col p-4 my-4 rounded-lg mt-12 hover:scale-105 duration-300">
+          <img
+            className="w-200 mx-auto mt-[-5rem] rounded-xl"
+            src={shoe1}
+            alt="/"
+          />
+          <p className="text-center pb-4 border-gray-600 border-b mt-10 text-white text-4xl font-bold">
+            {items.map((link) => (
+              <p>{link.cost}</p>
+            ))}
+          </p>
+          <div className="text-center font-medium">
+            <p className="py-2 mx-8 text-white font-bold mt-8">
+              {items.map((link) => (
+                <p>{link.desc}</p>
+              ))}
+            </p>
+          </div>
+          <button className="bg-[#00df9a] w-[200px] text-[#090d28] rounded-md font-bold my-6 mx-auto px-6 py-3">
+            Buy Now
+          </button>
+        </div>
+        <div className="w-full bg-[#080b1b] shadow-xl flex flex-col p-4 my-4 rounded-lg mt-12 hover:scale-105 duration-300">
+          <img
+            className="w-200 mx-auto mt-[-5rem] rounded-xl"
+            src={shoe1}
+            alt="/"
+          />
+          <p className="text-center pb-4 border-gray-600 border-b mt-10 text-white text-4xl font-bold">
+            {items.map((link) => (
+              <p>{link.cost}</p>
+            ))}
+          </p>
+          <div className="text-center font-medium">
+            <p className="py-2 mx-8 text-white font-bold mt-8">
+              {items.map((link) => (
+                <p>{link.desc}</p>
+              ))}
+            </p>
           </div>
           <button className="bg-[#00df9a] w-[200px] text-[#090d28] rounded-md font-bold my-6 mx-auto px-6 py-3">
             Buy Now
---
+++ src/constant/index.js
@@ -0,0 +1,14 @@
+// import  { shoe1} from "../assets/shoe1.jpeg";
+// import  { shoe2} from "../assets/shoe2.jpeg";
+// import  { shoe3} from "../assets/shoe3.jpeg";
+// import  { shoe4} from "../assets/shoe4.jpeg";
+// import  { shoe5} from "../assets/shoe5.jpeg";
+// import  { shoe6} from "../assets/shoe6.jpeg";
+
+
+export const items = [
+  {
+    desc: "Brand New Shoe, Japan",
+    cost: "$149.99",
+  },
+];
